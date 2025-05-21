e6850522bbec2099eb162d75a763566961c4269e
A15229 <A15229@lab.aca>
2025-05-21 12:02:33 -0500

    cutti

--- src/components/menu/Navbar.tsx
+++ src/components/menu/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar: React.FC<NavbarProps> = ({
   const navigate = useNavigate();
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-      <a className="navbar-brand text-white me-4" onClick={() => onFilterCategory('All')}>GameStore</a>
+      <a className="navbar-brand text-white me-4" onClick={() => onFilterCategory('All')}>RetroGames</a>
       <div className="navbar-nav">
         <a className="nav-link" onClick={() => onFilterCategory('Mejor valorados')}>Mejor Valorados</a>
         <a className="nav-link" onClick={() => onFilterCategory('Más vendidos')}>Más vendidos</a>
