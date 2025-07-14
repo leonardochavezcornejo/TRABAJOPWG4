92b17b38029f57a7168186d7cca116a9627b30e0
leonardochavezcornejo <leonardo.chavez.cornejo@gmail.com>
2025-07-14 02:59:46 -0500

    chiqui

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/TRABAJOPWG4/',
   plugins: [react()],
-  base: '/TRABAJOPWG4/'
 })
